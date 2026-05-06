3a87de1a5279f18695d05f0f6b76a68bca52a802
Aditya743golu <163764818+Aditya743golu@users.noreply.github.com>
2026-05-06 13:59:31 +0530

    Add problem2 solution

--- problem2.c
+++ problem2.c
@@ -1,42 +1,56 @@
+// Problem 2: Array Transformation Cost Minimization
+// Given an array, you can add or subtract K from any element.
+// Find the minimum number of operations to make all elements equal,
+// or print -1 if it is not possible.
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int compare(const void a, const void b) {
-return ((int)a - (int)b);
+int compare(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
 }
 
 int main() {
-int n;
-scanf("%d", &n);
+    int n;
+    scanf("%d", &n);
 
-int arr[100005];
+    int arr[100005];
 
-for (int i = 0; i < n; i++) {
-    scanf("%d", &arr[i]);
-}
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
 
-int k;
-scanf("%d", &k);
+    int k;
+    scanf("%d", &k);
 
-// Check feasibility
-for (int i = 1; i < n; i++) {
-    if ((arr[i] - arr[0]) % k != 0) {
-        printf("-1\n");
+    if (k == 0) {
+        for (int i = 1; i < n; i++) {
+            if (arr[i] != arr[0]) {
+                printf("-1\n");
+                return 0;
+            }
+        }
+        printf("0\n");
         return 0;
     }
-}
 
-// Sort array
-qsort(arr, n, sizeof(int), compare);
+    // Check feasibility
+    for (int i = 1; i < n; i++) {
+        if ((arr[i] - arr[0]) % k != 0) {
+            printf("-1\n");
+            return 0;
+        }
+    }
 
-int median = arr[n / 2];
-int operations = 0;
+    qsort(arr, n, sizeof(int), compare);
 
-for (int i = 0; i < n; i++) {
-    operations += abs(arr[i] - median) / k;
-}
+    int median = arr[n / 2];
+    int operations = 0;
 
-printf("%d\n", operations);
-return 0;
+    for (int i = 0; i < n; i++) {
+        operations += abs(arr[i] - median) / k;
+    }
 
+    printf("%d\n", operations);
+    return 0;
 }
